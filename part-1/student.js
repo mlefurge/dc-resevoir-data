d0f37f4c202cbf2c0260bcd12f0cd30e9c382411
André <email": null>
2015-10-14 10:40:01 -0400

    add letterGrade function

--- part-1/student.js
+++ part-1/student.js
@@ -1,9 +1,28 @@
 var Student = function(name, scores){
   this.firstName = name;
   this.scores = scores;
-  this.averageScore = function(){
+};
+
+Student.prototype.averageScore = function(){
     var sum = this.scores.reduce(function(a, b) {return a + b; });
     var avg = sum / this.scores.length;
     return Math.floor(avg);
+  };
+
+Student.prototype.letterGrade = function(){
+    if (this.averageScore() >= 90){
+      return "A";
     }
-};
+    else if (this.averageScore() >= 80 && this.averageScore() < 90){
+      return "B";
+    }
+    else if (this.averageScore() >= 70 && this.averageScore() < 80){
+      return "C";
+    }
+    else if (this.averageScore() >= 60 && this.averageScore() < 70){
+      return "D";
+    }
+    else {
+      return "F";
+    }
+  };
